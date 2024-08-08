e7746ab9fb74866e93b9600112e6df33ca56cf50
Sylicium <contact@sylicium.fr>
2024-08-08 03:07:23 +0200

    First commit

---
+++ production/lyntr-toolbox.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Lyntr Toolbox
+// @version      1.4.2
+// @namespace    https://lyntr.com/
+// @description  A toolbox for small and medium changes for lyntr.com ! What is it ? -> https://youtu.be/-D2L3gHqcUA
+// @author       Sylicium
+// @match        https://lyntr.com/*
+// @icon         https://lyntr.com/favicon.ico
+// @grant        none
+// @downloadURL https://update.greasyfork.org/scripts/392920/Krunker%20Rzoxs%20Hack.user.js
+// @updateURL https://update.greasyfork.org/scripts/392920/Krunker%20Rzoxs%20Hack.meta.js
+// ==/UserScript==
