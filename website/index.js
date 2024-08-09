26c6b23217c34fb70089d06de2e44aa042842b5c
Sylicium <contact@sylicium.fr>
2024-08-10 01:01:11 +0200

    Update website

--- website/index.js
+++ website/index.js
@@ -17,19 +17,64 @@ const _DIRNAME_ = __dirname + "/site";
 // Make the app.get("*") to parse every request
 app.get('*', (req, res) => {
 
-    console.log(req.url)
+    let log = `[lyntr+:${PORT}][${(new Date()).toLocaleString("FR-fr")}][${req.connection.remoteAddress}] ${req.method} -> ${req.path}   [${req.headers['user-agent']}]`
+
+    // Rewrite line adding colors
+    let colors = {
+        "GET": "\x1b[32m",
+        "POST": "\x1b[33m",
+        "PUT": "\x1b[34m",
+        "DELETE": "\x1b[31m",
+        "PATCH": "\x1b[35m",
+        "OPTIONS": "\x1b[36m",
+        "HEAD": "\x1b[37m",
+        "TRACE": "\x1b[38m",
+        "CONNECT": "\x1b[39m",
+    }
+    let colors2 = {
+        "bold": "\x1b[1m",
+        "red": "\x1b[31m",
+        "green": "\x1b[32m",
+        "yellow": "\x1b[33m",
+        "blue": "\x1b[34m",
+        "magenta": "\x1b[35m",
+        "cyan": "\x1b[36m",
+        "white": "\x1b[37m",
+        "gray": "\x1b[90m",
+        "reset": "\x1b[0m",
+    }
+    let clrz = (what, value) => {
+        if(what == "method"){
+            return `${colors2.bold}${colors[value]}${value}${colors2.reset}`
+        } else if(what == "path"){
+            return `${colors2.green}${value}${colors2.reset}`
+        } else if(what == "user-agent"){
+            return `${colors2.yellow}${value}${colors2.reset}`
+        } else {
+            return value
+        }
+             
+    }
+    let log2 = `[lyntr+:${PORT}][${(new Date()).toLocaleString("FR-fr")}][${req.connection.remoteAddress}] ${clrz("method", req.method)} -> ${clrz("path", req.path).padEnd(60, " ")}   [${clrz("user-agent", req.headers['user-agent'])}]`
+
+    console.log(log2)
 
     // If startsWith /assets/ return the file
     if (req.url.startsWith("/assets/")) {
         return res.sendFile(_DIRNAME_ + req.path)
     }
-    // If startsWith /api/ return the file
     if (req.url.startsWith("/api/")) {
         return;
     }
     if (req.url === "/") {
         return res.sendFile(_DIRNAME_ + "/index.html")
+    }    
+    if(fs.existsSync(_DIRNAME_ + req.path) && req.path.endsWith(".html")){
+        return res.sendFile(_DIRNAME_ + req.path)
+    } else if(fs.existsSync(_DIRNAME_ + req.path + ".html")){
+        return res.sendFile(_DIRNAME_ + req.path + ".html")
     }
+
     return res.sendFile(_DIRNAME_ + "/404.html")
 })
 
