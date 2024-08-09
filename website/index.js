23e9a3fcd0f4e4ab891942510ed143796b941ac3
Sylicium <contact@sylicium.fr>
2024-08-10 00:33:05 +0200

    Update website

--- website/index.js
+++ website/index.js
@@ -1,16 +1,45 @@
 
 // Simple web server
+const axios = require("axios")
+const fs = require("fs")
 const express = require('express');
-const app = express();
-const fs = require('fs');
-const port = 3000;
+// const bodyParser = require('body-parser');
+const app = express(); // Uncaught (in promise) TypeError: Failed to execute 'fetch' on 'Window': Request with GET/HEAD method cannot have body.
+app.use(express.urlencoded())
+app.use(express.json())
+//app.use(bodyParser.json({ type: 'application/*+json' }))
+const serv = require('http').createServer(app);
+
+const PORT = 88;
+const _DIRNAME_ = __dirname + "/site";
 
 
-
+// Make the app.get("*") to parse every request
 app.get('*', (req, res) => {
-    if(fs.existsSync(`./public${req.url}.html`)){
-        res.sendFile(`./public${req.url}.html`, {root: __dirname});
-    } else {
-        res.sendFile('./public/404.html', {root: __dirname});
+
+    console.log(req.url)
+
+    // If startsWith /assets/ return the file
+    if (req.url.startsWith("/assets/")) {
+        return res.sendFile(_DIRNAME_ + req.path)
     }
+    // If startsWith /api/ return the file
+    if (req.url.startsWith("/api/")) {
+        return;
+    }
+    if (req.url === "/") {
+        return res.sendFile(_DIRNAME_ + "/index.html")
+    }
+    return res.sendFile(_DIRNAME_ + "/404.html")
 })
+
+app.all('/api', (req, res) => {
+    return res.send({
+        status: 400,
+        message: "Bad Request"
+    })
+})
+
+serv.listen(PORT, () => {
+    console.log(`Server listening on port ${serv.address().port}`);
+});
